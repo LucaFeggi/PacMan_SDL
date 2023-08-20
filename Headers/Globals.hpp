2e8cac95c0129c639f0aa47bd33ab62fa2c5730c
TheBeardedQuack <TheBeardedQuack@gmail.com>
2023-08-20 11:57:15 +0100

    Created implementation file for Globals header

--- Headers/Globals.hpp
+++ Headers/Globals.hpp
@@ -1,3 +1,10 @@
+#pragma once
+
+#include <string>
+
+#include <SDL.h>
+#include <SDL_ttf.h>
+
 const unsigned char BoardWidth = 28;
 const unsigned char BoardHeight = 36;
 const unsigned char BlockSize32 = 32;
@@ -59,55 +66,47 @@ const SDL_Color Cyan = {0x00, 192, 0xff};
 const SDL_Color Pink = {0xff, 192, 203};
 const SDL_Color Orange = {0xff, 128, 0x00};
 
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
-SDL_Rect* CurrentClip = NULL;
-TTF_Font* Font = NULL;
-TTF_Font* LittleFont = NULL;
+extern SDL_Window* window;
+extern SDL_Renderer* renderer;
+extern SDL_Rect* CurrentClip;
+extern TTF_Font* Font;
+extern TTF_Font* LittleFont;
 
 enum BlockType{
-	Wall, Door, Pellet, Energizer, Nothing
+	Wall,
+	Door,
+	Pellet,
+	Energizer,
+	Nothing,
 };
 
 enum Direction{
-	Right, Up, Left, Down, Nowhere
+	Right,
+	Up,
+	Left,
+	Down,
+	Nowhere,
 };
 
 enum EntityType{
-	ePacMan, eBlinky, eInky, ePinky, eClyde, Noone
+	ePacMan,
+	eBlinky,
+	eInky,
+	ePinky,
+	eClyde,
+	Noone,
 };
 
-void InitializeSDL(){
-	SDL_Init(SDL_INIT_VIDEO);
-	window = SDL_CreateWindow("PacMan", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WindowWidth, WindowHeight, SDL_WINDOW_SHOWN);
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
-	int imgFlags = IMG_INIT_PNG;
-	IMG_Init(imgFlags);
-	TTF_Init();
-	Font = TTF_OpenFont("Fonts/emulogic.ttf", BlockSize24);
-	LittleFont = TTF_OpenFont("Fonts/VpPixel.ttf", 20);
-	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024);
-}
+void InitializeSDL(
+	void
+);
 
-void CloseSDL(){
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	window = NULL;
-	renderer = NULL;
-	CurrentClip = NULL;
-	IMG_Quit();
-	TTF_Quit();
-	Mix_Quit();
-	SDL_Quit();
-}
+void CloseSDL(
+	void
+);
 
-void InitFrames(const unsigned char TotalFrames, SDL_Rect SpriteClips[], unsigned char CurrentBlockSize = BlockSize32){
-	unsigned short counter = 0;
-	for(unsigned char i = 0; i < TotalFrames; i++){
-		SpriteClips[i].x = counter;
-		SpriteClips[i].y = 0;
-		SpriteClips[i].w = CurrentBlockSize;
-		SpriteClips[i].h = CurrentBlockSize;
-		counter += CurrentBlockSize;
-	}
-}
+void InitFrames(
+	const unsigned char TotalFrames,
+	SDL_Rect SpriteClips[],
+	unsigned char CurrentBlockSize = BlockSize32
+);
