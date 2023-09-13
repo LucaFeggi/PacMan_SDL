981d258e8159c4020637e271bf41c3833ac40743
Raul <raul@gmx.se>
2023-09-13 13:02:23 +0200

    Keyboard logic moved from main.cpp into Game.hpp. Simpler main()

--- main.cpp
+++ main.cpp
@@ -33,34 +33,22 @@ int main(int argc, char* args[]){
 
 	Game mGame;
 	Timer GameTimer;
-	SDL_Event event;
-	bool quit = false;
+	
+    bool quit = false;
 	unsigned short StartTicks = 4500;
-	std::vector<unsigned char> mover;
-	mover.push_back(Right);
-	GameTimer.Start();
+
+    GameTimer.Start();
 	mGame.mSound.PlayIntro();
 
 	while(!quit){
 
 		double IterationStart = SDL_GetPerformanceCounter();
 
-		while(SDL_PollEvent(&event) != 0){
-			if(event.type == SDL_QUIT)
-				quit = true;
-			if(event.key.state == SDL_PRESSED){
-				if((event.key.keysym.sym == SDLK_RIGHT || event.key.keysym.sym == SDLK_d))			mover.push_back(Right);
-				else if((event.key.keysym.sym == SDLK_UP || event.key.keysym.sym == SDLK_w)) 		mover.push_back(Up);
-				else if((event.key.keysym.sym == SDLK_LEFT || event.key.keysym.sym == SDLK_a))	mover.push_back(Left);	
-				else if((event.key.keysym.sym == SDLK_DOWN || event.key.keysym.sym == SDLK_s))	mover.push_back(Down);	
-				if(mover.size() > 2)
-					mover.erase(mover.begin() + 1);
-			}
-		}
+        quit = mGame.keyboardProcess();
 
 		SDL_RenderClear(renderer);
 
-		if(mGame.Process(GameTimer, mover, StartTicks)){
+		if(mGame.Process(GameTimer, StartTicks)){
 			mGame.Draw();
 			SDL_RenderPresent(renderer);
 		}
