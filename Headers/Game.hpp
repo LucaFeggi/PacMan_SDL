e86118b23661e48d89f2301fd4f6b7a0955a087b
Raul <raul@gmx.se>
2023-09-16 14:30:17 +0200

    headers moved into Game.cpp

--- Headers/Game.hpp
+++ Headers/Game.hpp
@@ -3,20 +3,20 @@
 #include <vector>
 #include <cmath>
 
-#include "../Headers/Globals.hpp"
-#include "../Headers/Texture.hpp"
-#include "../Headers/Timer.hpp"
-#include "../Headers/Position.hpp"
-#include "../Headers/Entity.hpp"
-#include "../Headers/Pac.hpp"
-#include "../Headers/Ghost.hpp"
-#include "../Headers/Blinky.hpp"
-#include "../Headers/Inky.hpp"
-#include "../Headers/Pinky.hpp"
-#include "../Headers/Clyde.hpp"
-#include "../Headers/Fruit.hpp"
-#include "../Headers/Board.hpp"
-#include "../Headers/Sound.hpp"
+#include "./Globals.hpp"
+#include "./Texture.hpp"
+#include "./Timer.hpp"
+#include "./Position.hpp"
+#include "./Entity.hpp"
+#include "./Pac.hpp"
+#include "./Ghost.hpp"
+#include "./Blinky.hpp"
+#include "./Inky.hpp"
+#include "./Pinky.hpp"
+#include "./Clyde.hpp"
+#include "./Fruit.hpp"
+#include "./Board.hpp"
+#include "./Sound.hpp"
 
 class Game{
 	public:
